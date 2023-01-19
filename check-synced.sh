5b4a8e31aea973ca25bd56582706d87388c9baf3
gStart9 <george@start9labs.com>
2023-01-18 20:19:12 -0700

    One more check-synced.sh touchup

--- check-synced.sh
+++ check-synced.sh
@@ -29,10 +29,7 @@ if [ "$b_block_count" = "null" ]; then
     echo "Error ascertaining Bitcoin block count: $b_gbc_error" >&2
     exit 60
 else
-    #the netcat method below fetches metrics from the prometheus rpc, and querying :50001 doesn't work until electrs is synced, so we'll curl directly instead.
-    #features_res=$(echo '{"jsonrpc": "2.0", "method": "server.features", "params": ["", "1.4"], "id": 0}' | netcat -q 1 127.0.0.1 50001)
-    #This doesn't ever seem to correspond to actual compaction events.  We'll determine compaction another way.
-    #compaction_res=$(echo -e "$features_res" | grep num-running-compactions | sed "s/\s$//g" | grep " [^0]$"|awk '{print $NF}'|head -1)
+    #Gather keys/values from prometheus rpc:
     curl_res=$(curl -sS localhost:4224)
     error_code=$?
     
@@ -40,7 +37,9 @@ else
         echo "Error contacting the electrs Prometheus RPC" >&2
         exit $error_code
     fi
-
+    
+    #compaction_res=$(echo -e "$features_res" | grep num-running-compactions | sed "s/\s$//g" | grep " [^0]$"|awk '{print $NF}'|head -1)
+    #^The prometheus RPC's num-running-compactions key doesn't seem to correspond to actual compaction events, so we'll determine compaction by another, dumber but accurate method:
     compaction_res=$(tail -2 /data/db/bitcoin/LOG|head -1|grep "compaction_job\.cc"|wc -l)
     if [[ $compaction_res -eq 1 ]] ; then
         echo "Finishing database compaction... This could take a some hours depending on your hardware." >&2
