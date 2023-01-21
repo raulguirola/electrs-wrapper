52e423524df6d7705160ad8880244b1933a1c8d4
gStart9 <george@start9labs.com>
2023-01-21 12:43:24 -0700

    Add new eOS rsync backup functionality

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -3,3 +3,4 @@ export { dependencies } from "./services/dependencies.ts";
 export { properties } from "./services/properties.ts";
 export { getConfig } from "./services/getConfig.ts";
 export { migration } from "./services/migrations.ts";
+export { createBackup, restoreBackup } from "./services/procedures/backups.ts";
